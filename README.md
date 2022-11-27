e5070544f9767f6f2950acfff5ba01c32007cb3e
ethanhocker <ehocker5547@gmail.com>
2022-11-26 21:07:00 -0500

    added search activity start

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 Name: Ethan Hocker and Michael Hallabrin
 
-Time: Ethan Hocker: 5 hours
+Time: Ethan Hocker: 7.5 hours
 
 Comments:
 *  This Stack Overflow post showed how to make a line in a constraint layout: https://stackoverflow.com/questions/64111669/i-need-a-line-dividing-2-nested-constraint-layouts-exactly-in-the-center 
--- app/src/main/java/com/cs386p/investment_buddy/MainActivity.kt
+++ app/src/main/java/com/cs386p/investment_buddy/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.cs386p.investment_buddy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-
+import com.cs386p.investment_buddy.databinding.ActivityMainBinding
+import com.cs386p.investment_buddy.databinding.ContentMainBinding
+import com.cs386p.investment_buddy.ui.StockSearch
 
 class MainActivity : AppCompatActivity() {
     val api_key = "RUXI1LX1OCUM137J" // TODO: Move this, just noting this here for now
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding : ContentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        binding = activityMainBinding.contentMain
 
         // Set default mode for app to dark mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        // Start StockSearch activity when analyze text view is clicked
+        binding.analyze.setOnClickListener {
+            val stockSearchClass = StockSearch()
+            val intent = Intent(this, stockSearchClass::class.java)
+            this.startActivity(intent)
+        }
 
         viewModel.symbolSearch() //TODO: Delete me - just for testing
     }
